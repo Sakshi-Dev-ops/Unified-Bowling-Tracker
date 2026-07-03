e4845518fd38499d6bcb16e636aac2ca2a8d17b8
Sakshi-Dev-ops <122nsps@gmail.com>
2026-07-03 18:21:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 
 # Page config
 st.set_page_config(
-    page_title="🎳 PHHS Bowling Tracker",
+    page_title=" PHHS Bowling Tracker",
     page_icon="🎳",
     layout="wide",
     initial_sidebar_state="expanded"
